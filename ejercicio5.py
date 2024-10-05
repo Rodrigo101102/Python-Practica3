d28c97d8071b39b90e168a364d06d98f7f41d4c6
melisa176 <carolinacastillon27@gmail.com>
2024-10-05 14:34:26 -0500

    Ejercicio 5

--- ejercicio5.py
+++ ejercicio5.py
@@ -1,1 +1,20 @@
 #Melisa Castillon 
+def suma_digitos():
+    while True:
+        try:
+            numero=int(input("Ingrese el numero: " ))
+            if numero<0:
+                print("Ingrese un numero positivo")
+            else:
+                break
+        except ValueError:
+            print("Ingrese un numero entero")
+    str_numero=str(numero)
+    while len(str_numero)>1:
+        suma=0
+        for i in str_numero:
+            suma+=int(i)
+        str_numero=str(suma)
+    return int(str_numero)
+resultado=suma_digitos()
+print("El resultado de la suma de dígitos es:", resultado)
